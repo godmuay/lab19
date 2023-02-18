a0dd02157294de79dd5e307073e5db3be8ea55c3
godmuay <apinyakasi19@gmail.com>
2023-02-18 22:31:34 +0700

    19.1-f

--- lab19_1.cpp
+++ lab19_1.cpp
@@ -20,20 +20,60 @@ string toUpperStr(string x){
     return y;
 }
 
-void importDataFromFile(){
-
+void importDataFromFile(string filename,vector<string> &names,vector<int> &scores,vector<char> &grades){
+    ifstream File(filename);
+    string name;
+    char Name[100];
+    int a,b,c;
+    char format[] = "%[^:]: %d %d %d";
+    while (getline(File,name))
+    {
+        sscanf(name.c_str(),format,Name,&a,&b,&c);
+        names.push_back(Name);
+        scores.push_back(a+b+c);
+        grades.push_back(score2grade(a+b+c));
+    }
 }
 
-void getCommand(){
-
+void getCommand(string &command,string &key){
+    cout << "Please input your command: " ;
+    string text;
+    getline(cin,text);
+    char format[] = "%s %[^:]";
+    char textcmd[10] , textkey[100]; 
+    sscanf(text.c_str(),format,textcmd,textkey);
+    command = textcmd;
+    key = textkey;
 }
 
-void searchName(){
-
+void searchName(vector<string> names,vector<int> scores,vector<char> grades,string key){
+    int N = names.size();
+    bool check = false;
+    cout << "---------------------------------\n";
+    for(int i=0;i<N;i+=1){
+        if(key == toUpperStr(names[i])){
+            cout << names[i] << "'s score = " << scores[i] << endl;
+            cout << names[i] << "'s grade = " << grades[i] << endl;
+            check = true;
+        }
+    }
+    if(check == false){cout << "Cannot found." << endl;}
+    cout << "---------------------------------\n";
 }
 
-void searchGrade(){
-
+void searchGrade(vector<string> names,vector<int> scores,vector<char> grades,string key){
+    string x;
+    bool check = false;
+    int N = grades.size();
+    cout << "---------------------------------\n";
+    for(int i=0;i<N;i+=1){
+        x = grades[i];
+        if(key == toUpperStr(x)){
+            cout << names[i] << " (" << scores[i] << ")" << endl;
+            check = true;
+        }
+    }if(check == false){cout << "Cannot found." << endl ;}
+    cout << "---------------------------------\n";
 }
 
 
